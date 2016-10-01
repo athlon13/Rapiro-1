863448bc2c4468393039de74ec0cf05234d44d6c
Takashi Kojo <kojo@wolfsssl.com>
2016-10-02 07:25:32 +0900

    init_choreo.py

---
+++ tools/init_choreo.py
@@ -0,0 +1,26 @@
+from __future__ import division
+import os
+import sys
+import time
+import re
+import pickle
+import json
+import codecs
+
+from choreo_data import ChoreoData
+
+#print json.dumps(ChoreoData)
+c = []
+for c in ChoreoData:
+    print str(c)
+    fout = codecs.open('./choreo/' + str(c), 'w')
+    for posture in ChoreoData[c]:
+        pos = posture['pos']
+        line_pos = 'p -s ' + str(posture['period']*100) + ' '
+        for i in range(0, len(pos)):
+            line_pos += str(i) + ':' + str(pos[i]) + " "
+        print line_pos
+        fout.write(line_pos+'\n')
+    fout.close()
+    
+#print json.dumps(ChoreoData, sort_keys=True,indent=4, separators=(',', ': '))
