019912b68e15b5e74bf8f0f14ed34bc18ac36518
Takashi Kojo <kojo@wolfsssl.com>
2016-10-01 18:44:26 +0900

    Split off getchar file

---
+++ getch.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+############################################################
+# Simple demo of of the PCA9685 PWM servo/LED controller library.
+# This will move channel 0 from min to max position repeatedly.
+# Author: Tony DiCola
+# License: Public Domain
+############################################################
+
+############################################################
+# Getch extention for file input
+# Author: Nobuyuki Saji
+# License: 
+############################################################
+
+############################################################
+# imports
+
+import sys
+
+############################################################
+# class definitions
+
+class Getch:
+    def __init__(self, path=None):
+        try:
+            self.tty = _GetchWindows()
+        except ImportError:
+            self.tty = _GetchUnix()
+        self.stack = []
+        self.files = [path]
+        self.mode = path
+        self.impl = _GetchFile(path) if path else self.tty
+
+    # 'line' reads characters until CR/LF comes and returns line string
+    def __call__(self,line=False):
+        if not line: return self.impl()
+
+        buf = []
+        c = self.impl()
+        while c not in ('\r','\n'):
+            if c in ('\b','\x7f'): buf.pop()
+            else: buf.append(c)
+            if not self.mode: sys.stdout.write(c)
+            c = self.impl()
+        return ''.join(buf)
+
+    # Push nested input context (tty or file)
+    def push(self, path=None):
+        if path and path in self.files:
+            raise Exception("Duplicate nested file: "+path)
+
+        self.stack.append(self.impl)
+        self.files.append(path)
+        self.mode = path
+        self.impl = _GetchFile(path) if path else self.tty
+
+    # Pop nested input context (tty or file) if possible
+    def close(self):
+        if len(self.stack) > 0:
+            self.impl = self.stack.pop()
+            self.files.pop()
+            self.mode = self.files[-1]
+            return True
+        return False
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+class _GetchFile:
+    def __init__(self, path):
+        with open(path, 'r') as f:
+            self.clist = sum([list(x) for x in f],[])
+
+    def __call__(self):
+        if len(self.clist)==0: return None
+        return self.clist.pop(0)
+
+############################################################
+# quick test
+
+if __name__ == "__main__":
+    import sys
+    getch = Getch(sys.argv[1]) if len(sys.argv)>=2 else Getch()
+    print("Verbose: "+str(not getch.mode))
+    while True:
+        c = getch()
+        if c is None or c=='q': break
+        print(c)
--- rapiro_adjust.py
+++ rapiro_adjust.py
@@ -1,151 +1,251 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
 
+############################################################
 # Simple demo of of the PCA9685 PWM servo/LED controller library.
-# This will move channel 0 from min to max position repeatedly.
+# This will move channel 0 from min to max posture repeatedly.
 # Author: Tony DiCola
 # License: Public Domain
-#from __future__ import division
-import time
-#import pickle
+############################################################
+
+############################################################
+# Command control feature for servo/LED
+# Author: Takashi Kojo
+# License: 
+############################################################
+
+############################################################
+# imports
+
+from __future__ import division
+import os
 import sys
-import RPi.GPIO as GPIO
+import time
+import re
+import pickle
+import json
 
-class _Getch:
-    def __init__(self):
-        try:
-            self.impl = _GetchWindows()
-        except ImportError:
-            self.impl = _GetchUnix()
+# Import the PCA9685 module.
+import Adafruit_PCA9685
 
-    def __call__(self): return self.impl()
+# Getch
+from getch import Getch
 
-class _GetchUnix:
-    def __init__(self):
-        import tty, sys
+############################################################
+# constants
 
-    def __call__(self):
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
+# Uncomment to enable debug output.
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
 
-class _GetchWindows:
-    def __init__(self):
-        import msvcrt
+# Initialise the PCA9685 using the default address (0x40).
+pwm = Adafruit_PCA9685.PCA9685()
 
-    def __call__(self):
-        import msvcrt
-        return msvcrt.getch()
+# Alternatively specify a different address and/or bus:
+#pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
 
-min_duty = 1.0
-max_duty = 15.0
-max_degree = 180.0
+# Configure min and max servo pulse lengths
+servo_min = 150  # Min pulse length out of 4096
+servo_max = 600  # Max pulse length out of 4096
 
-#ch_pin = [8, 3, 5, 7, 10, 11,13,15,18, 19,21,23,26,32,29,31,33,35,37,40]
-ch_pin = [8, 3, 5, 7, 10, 11,13,15,18, 19,21,23]
-pwm  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# Rapiro status dumpfile
+C_RAPIRO_INIT = "rapiro.init"
 
-def init_servo():
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setwarnings(False)
+#  0: L foot pitch
+#  1: L foot yaw
+#  2: L hand grip
+#  3: L shoulder pitch
+#  4: L shoulder roll
+#  5: waist yaw
+#  6: head yaw
+#  7: R shoulder roll
+#  8: R shoulder pitch
+#  9: R hand grip
+# 10: R foot yaw
+# 11: R foot pitch
 
-	ch_num = len(ch_pin)
-	print str(ch_num) + " Channels"
+C_PARTS_LIST = ['foot_p_l','foot_y_l','hand_l','shld_p_l','shld_r_l',
+                'waist','head',
+                'shld_r_r','shld_p_r','hand_r','foot_y_r','foot_p_r']
+C_PARTS_TO_INDEX = {}
+C_INDEX_TO_PARTS = {}
+for i in range(0,len(C_PARTS_LIST)):
+    C_PARTS_TO_INDEX[C_PARTS_LIST[i]] = i
+    C_INDEX_TO_PARTS[i] = C_PARTS_LIST[i]
 
-	for i in range(0, ch_num):
-        	print "Channel:" + str(ch_pin[i])
-        	GPIO.setup(ch_pin[i], GPIO.OUT)
-        	pwm[i] = GPIO.PWM(ch_pin[i], 50.0)
-        	pwm[i].start(ch_pin[i])
-        	pwm[i].ChangeDutyCycle((max_duty+min_duty)/2.0)
+C_CHOREO_DIR = 'choreo'
 
-def set_servo(ch, degree):
-	duty = (max_duty - min_duty)/max_degree*degree
-	p = pwm[ch]
-	p.ChangeDutyCycle(duty)
+############################################################
+# Helper function to make setting a servo pulse width simpler.
 
-def smoothMove(ch, from_pos, to_pos):
-	if from_pos < to_pos:
-		delta = 1
-	else:
-		delta = -1
+def set_servo_pulse(channel, pulse, verbose=True):
+    pulse_length = 1000000    # 1,000,000 us per second
+    pulse_length //= 60       # 60 Hz
+    if verbose: print('{0}us per period'.format(pulse_length))
+    pulse_length //= 4096     # 12 bits of resolution
+    if verbose: print('{0}us per bit'.format(pulse_length))
+    pulse *= 1000
+    pulse //= pulse_length
+    pwm.set_pwm(channel, 0, pulse)
 
-	p = from_pos
-        while True:
-                set_servo(ch, 0, p)
-                if p == to_pos:
-                         break
+def unitMove(ch, p, verbose=False):
+    pwm.set_pwm(ch, 0, p)
+    if verbose: print('move ch:'+str(ch)+" "+str(p))
+    return p
+
+def smoothMove(ch, from_pos, to_pos, sleep=0.01, verbose=False):
+    delta = 1 if from_pos < to_pos else -1
+    for p in range(from_pos, to_pos+delta, delta):
+        unitMove(ch, p)
+        time.sleep(sleep)
+    if verbose: print('smooth ch:'+str(ch)+" "+str(from_pos)+"=>"+str(to_pos))
+    return to_pos - from_pos
+
+def fullSwing(ch, cur, min, max, verbose=False):
+    if verbose: print('swing ch:'+str(ch)+" "+str(cur)+"=>"+str(min)+"=>"+str(max)+"=>"+str(cur))
+    smoothMove(ch, cur, min, verbose=verbose)
+    smoothMove(ch, min, max, verbose=verbose)
+    smoothMove(ch, max, cur, verbose=verbose)
+    return 0
+
+def printhelp(verbose=True):
+    if not verbose:
+        print('Type H for help')
+        return
+    print('Channel Settings:')
+    print('  0:foot_p_l, 1:foot_y_l, 2:hand_l, 3:shld_p_l, 4:shld_r_l, 5:waist')
+    print('  6:head, 7:shld_r_r, 8:shld_p_r, 9:hand_r, 10:foot_y_r, 11:foot_p_r')
+    print('  12:red, 13:green, 14:blue')
+    print('Commands:')
+    print('  h:-10, j:-1, k:+1, l:+10, g:full swing')
+    print('  m:set center, x:set max pos, n:set min pos')
+    print('  c:load choreo file, i:revert to tty (use in file), H:help, q:quit')
+    print('')
+
+############################################################
+# main procedure
+
+def mainproc(path=None,dumpfile=None):
+    # Set frequency to 60hz, good for servos.
+    pwm.set_pwm_freq(60)
+
+    printhelp()
+ 
+    if dumpfile:
+        print("Loading stat: " + dumpfile)
+        with open(dumpfile, 'rb') as f:
+            rapiro = pickle.load(f)
+    else:
+        rapiro = {"servo": {"pos": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                            "max": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                            "min": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
+                  "led": [{"r": 0,"g":0,"bit": 0}]}
+
+    servo = rapiro["servo"]
+    led   = rapiro["led"]
+    pos   = servo["pos"]
+    max   = servo["max"]
+    min   = servo["min"]
+    
+    for ch in range(0, len(pos)):
+        unitMove(ch, pos[ch])
+
+    # File or tty for input commands
+    getch = Getch(path=path)
+    
+    ch = 0
+    while True:
+        c = getch()
+        verbose = not getch.mode
+
+        # q, ^C: exit from control loop
+        if c is None or c in ('\x03','q'):
+            if getch.close(): continue
+            break
+        
+        # \r, \n: skip
+        if c in ('\r','\n'): continue
+        
+        # 1-9: select channel 1-15
+        if c in ('0','1','2','3','4','5','6','7','8','9'):
+            if c != '1':
+                ch = int(c)
+            else:
+                c2 = getch()
+                if c2 in ('0','1','2','3','4','5'):
+                    ch = int(c + c2)
                 else:
-                        p += delta
-                time.sleep(0.01)
+                    ch = 1
+            if verbose: print("set current channel=" + str(ch))
+            continue
 
-def fullSwing(ch, min, max):
-        curr = rapiro["servo"]["pos"][ch]
-	print str(ch) + "," +  str(curr)
-	smoothMove(ch, curr, min)
-	print str(ch) + "," +  str(min)
-	smoothMove(ch, min, max)
-	print str(ch) + "," +  str(max)
-	smoothMove(ch, max, curr)	
-	print str(ch) + "," +  str(curr)
+        # h: move-10, j: move-1, k: move+1, l: move+10
+        if   c == 'h':
+            pos[ch] -= 10
+            unitMove(ch, pos[ch], verbose=True)
+        elif c == 'j':
+            pos[ch] -= 1
+            unitMove(ch, pos[ch], verbose=True)
+        elif c == 'k':
+            pos[ch] += 1
+            unitMove(ch, pos[ch], verbose=True)
+        elif c == 'l':
+            pos[ch] +=10
+            unitMove(ch, pos[ch], verbose=True)
+        # m: force all parts to center posture
+        elif c == 'm':
+            mid = (servo_min+servo_max)//2
+            for i in range(0, 16):
+                unitMove(i, mid)
+                pos[i] = mid
+            if verbose: print("set all channels to " + str(mid))
+        # x: set current posture as maximum
+        elif c == 'x':
+            max[ch] = pos[ch]
+            if verbose: print("set max pos=" + str(pos[ch]))
+        # n: set current posture as minimum
+        elif c == 'n':
+            min[ch] = pos[ch]
+            if verbose: print("set min pos=" + str(pos[ch]))
+        # g: move cur->min->max->cur posture
+        elif c == 'g':
+            fullSwing(ch, pos[ch], min[ch], max[ch], verbose=True)
 
+        # c: dance with specified choreography file (nestable)
+        #   c filename
+        elif c == 'c':
+            if verbose: sys.stdout.write('type choreography file: ')
+            choreo = getch(line=True)
+            if verbose: print('')
+            choreo = re.sub(r'[^-.\w]','',choreo.strip())
+            getch.push(os.path.join(C_CHOREO_DIR,choreo))
 
-print('Moving , <- . || / -> _, 0 - 11 for channel, "q" to quit...')
-getch = _Getch()
+        # p: make posture simultaneously with multiple channel settings
+        #   p 1:100 2:200 3:300 ... (set 'ch:pos' pairs, '=' also allowed)
+        elif c == 'p':
+            if verbose: sys.stdout.write('type channel settings: ')
+            posture = getch(line=True)
+            if verbose: print('')
+            plist = map(lambda x:map(int,re.split(r'[=:]',x)),
+                        re.split(r' +',posture.strip()))
+            for ch,p in plist:
+                unitMove(ch, p, verbose=True)
 
-pos    = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-max    = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-min    = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-led    = [ { "r": 0, "g": 0, "b": 0}] 
-servo  = {"pos": pos, "max":max, "min":min }
-rapiro = {"servo":servo, "led":led}
+        # i: change command control to tty (use in choreo files)
+        elif c == 'i':
+            getch.push()
+            
+        # H: print help
+        elif c == 'H':
+            printhelp()
+        # other chars: error
+        else:
+            print("INVALID COMMAND:"+c)
+            printhelp(verbose=False)
 
-init_servo()
-if len(sys.argv) == 2:
-	print "Loading stat: " + sys.argv[1] 
-	with open(sys.argv[1], 'rb') as f:
-		rapiro = pickle.load(f) 
-
-ch = 0
-while True:
-	time.sleep(1.0)
-	c = "0"
-	#c = getch()
-	if   c == ',':
-		rapiro["servo"]["pos"][ch] -= 10
-	elif c == '.':
-                rapiro["servo"]["pos"][ch] -= 1
-	elif c == '/':
-                rapiro["servo"]["pos"][ch] += 1
-	elif c == '_':
-                rapiro["servo"]["pos"][ch] +=10
-	elif c == 'm':
-		for i in range(0, len(ch_pin)):
-			set_servo(i, 90.0)
-			rapiro["servo"]["pos"][i] = 90.0
-	elif c == 'x':
-		rapiro["servo"]["max"][ch] = rapiro["servo"]["pos"][ch]
-	elif c == 'n':
-		rapiro["servo"]["min"][ch] = rapiro["servo"]["pos"][ch]
-	elif c == 'g':
-		fullSwing(ch, rapiro["servo"]["min"][ch], rapiro["servo"]["max"][ch])
-	elif c == 'q':
-		break
-	elif int(c) in range(0, 10):
-		if int(c) != 1:
-			ch = int(c)
-		else:
-			c2 = getch()
-			if c2 in ["0","1","2","3","4","5","6","7","8","9"]:
-				ch = int(c + c2)
-			else:
-			    ch = 1
-	set_servo(ch, rapiro["servo"]["pos"][ch])
-	print str(ch) + ", " + str(rapiro["servo"]["pos"][ch])
-
-with open("rapiro.init", "wb") as f:
-	pickle.dump(rapiro, f)
+    with open(C_RAPIRO_INIT, "wb") as f:
+        pickle.dump(rapiro, f)
+ 
+if __name__ == "__main__":
+    print('Usage: %s [ choreo ] [ dump ]' % os.path.basename(sys.argv[0]))
+    mainproc(*sys.argv[1:])
